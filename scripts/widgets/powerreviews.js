229e1830a0ac8c879567c3058bf0a3cf492a9757
lakshmi_nair <lakshmi_nair@L01402.corp.volusion.com>
2015-04-16 10:02:08 -0500

    Code change after defect fixes.

--- scripts/widgets/powerreviews.js
+++ scripts/widgets/powerreviews.js
@@ -9,6 +9,7 @@ define(['modules/jquery-mozu', 'hyprlive',"modules/backbone-mozu",  "modules/mod
 	var zip_location;
 	var prStylesReview;
     var prMerchantStyles2;
+    var locale="en_US";
 		
     function writeProductListBoxes() {
         $('.mz-productlist').append('<link rel="stylesheet" href=\"'+prStylesReview+'\" type="text/css" id="prBaseStylesheet">');
@@ -24,42 +25,42 @@ define(['modules/jquery-mozu', 'hyprlive',"modules/backbone-mozu",  "modules/mod
             filter: 'productCode  eq ' + allInlineRatings.map(function() { return $(this).data('mzProductCode'); }).get().join(' or productCode  eq ')
         }).then(function(collection) {
             // turn from array of ids into map with key
-            var productsMap = collection.data.items.reduce(function(memo, item) {
+        var productsMap = collection.data.items.reduce(function(memo, item) {
                 memo[item.productCode] = item;
                 return memo;
             }, {});
-            allInlineRatings.each(function() {
-    			var $this = $(this);
-    			var productCode = $this.data('mzProductCode');
-                var data = productsMap[productCode];
-                if (data) {
-    			var fullReviewCount=data.fullReviews;
-    			var fullReviewCountText;
-    			if(data.fullReviews>1){
-    				fullReviewCountText="(" +data.fullReviews+"reviews)";
-    			}else if(data.fullReviews==1){ 
-    				fullReviewCountText="(" +data.fullReviews+"review)";
-    			}
+        allInlineRatings.each(function() {
+            var $this = $(this);
+            var productCode = $this.data('mzProductCode');
+            var data = productsMap[productCode];
+            if (data) {
+    		      var fullReviewCount=data.fullReviews;
+    			  var fullReviewCountText;
+    			  if(data.fullReviews>1){
+    			       fullReviewCountText="(" +data.fullReviews+"reviews)";
+    			  }else if(data.fullReviews==1){ 
+    			        fullReviewCountText="(" +data.fullReviews+"review)";
+    			  }
     		
-        	   if(data.averageDecimalRating>0 && data.merchantGrpId==merchantGroupId){
-                	$("#PRInlineRating-"+productCode).show();
-                	$("#PRInlineRating-"+productCode).find( ".pr-snippet-write-review").show();
-        		    $("#PRInlineRating-"+productCode).find( ".pr-snippet-read-reviews").show();
-        			$("#PRInlineRating-"+productCode).find(".pr-snippet-write-review").find("a.pr-snippet-link").attr("href", '/write-a-review?pageId='+productCode+'&merchantGroupId='+merchantGroupId+'&merchantId='+merchantId+'&siteId='+siteId+'&zipLocation='+zip_location);
-        			$("#PRInlineRating-"+productCode).find(".pr-snippet-read-reviews").find("a.pr-snippet-link").find("#pr-snippet-read-review-count").text(fullReviewCount);
-        			$("#PRInlineRating-"+productCode).find("#pr-snippet-rating-decimal").text(data.averageDecimalRating);
-        			$("#PRInlineRating-"+productCode).find("#pr-snippet-review-count").text(fullReviewCountText);
-        			$("#PRInlineRating-"+productCode).find("#pr-snippet-star-image").addClass("pr-stars").addClass("pr-stars-small").addClass("pr-stars-"+data.averageOverallRating+"-sm");
-        		}else{
-        		     $( "#PRInlineRating-"+productCode).show();
-        		    $("#PRInlineRating-"+productCode).find( ".pr-snippet-write-first-review").show();
-        			$("#PRInlineRating-"+productCode).find(".pr-snippet-write-first-review").find("a.pr-snippet-link").attr("href", '/write-a-review?pageId='+productCode+'&merchantGroupId='+merchantGroupId+'&merchantId='+merchantId+'&siteId='+siteId+'&zipLocation='+zip_location);
-        			$("#PRInlineRating-"+productCode).find("#pr-snippet-rating-decimal").text("0.0");
-        			$("#PRInlineRating-"+productCode).find("#pr-snippet-review-count").text("(No reviews)");
-        			$("#PRInlineRating-"+productCode).find("#pr-snippet-star-image").addClass("pr-stars").addClass("pr-stars-small").addClass("pr-stars-0_0-sm");
-        		}
-        		} else {
-        		    $( "#PRInlineRating-"+productCode).show();
+                	if(data.averageDecimalRating>0 && data.merchantGrpId==merchantGroupId){
+                        	$("#PRInlineRating-"+productCode).show();
+                        	$("#PRInlineRating-"+productCode).find( ".pr-snippet-write-review").show();
+                		    $("#PRInlineRating-"+productCode).find( ".pr-snippet-read-reviews").show();
+                			$("#PRInlineRating-"+productCode).find(".pr-snippet-write-review").find("a.pr-snippet-link").attr("href", '/write-a-review?pageId='+productCode+'&merchantGroupId='+merchantGroupId+'&merchantId='+merchantId+'&siteId='+siteId+'&zipLocation='+zip_location);
+                			$("#PRInlineRating-"+productCode).find(".pr-snippet-read-reviews").find("a.pr-snippet-link").find("#pr-snippet-read-review-count").text(fullReviewCount);
+                			$("#PRInlineRating-"+productCode).find("#pr-snippet-rating-decimal").text(data.averageDecimalRating);
+                			$("#PRInlineRating-"+productCode).find("#pr-snippet-review-count").text(fullReviewCountText);
+                			$("#PRInlineRating-"+productCode).find("#pr-snippet-star-image").addClass("pr-stars").addClass("pr-stars-small").addClass("pr-stars-"+data.averageOverallRating+"-sm");
+                	}else{
+                		    $( "#PRInlineRating-"+productCode).show();
+                		    $("#PRInlineRating-"+productCode).find( ".pr-snippet-write-first-review").show();
+                			$("#PRInlineRating-"+productCode).find(".pr-snippet-write-first-review").find("a.pr-snippet-link").attr("href", '/write-a-review?pageId='+productCode+'&merchantGroupId='+merchantGroupId+'&merchantId='+merchantId+'&siteId='+siteId+'&zipLocation='+zip_location);
+                			$("#PRInlineRating-"+productCode).find("#pr-snippet-rating-decimal").text("0.0");
+                			$("#PRInlineRating-"+productCode).find("#pr-snippet-review-count").text("(No reviews)");
+                			$("#PRInlineRating-"+productCode).find("#pr-snippet-star-image").addClass("pr-stars").addClass("pr-stars-small").addClass("pr-stars-0_0-sm");
+                	}
+        		}else {
+        		    $("#PRInlineRating-"+productCode).show();
         		    $("#PRInlineRating-"+productCode).find( ".pr-snippet-write-first-review").show();
         			$("#PRInlineRating-"+productCode).find(".pr-snippet-write-first-review").find("a.pr-snippet-link").attr("href", '/write-a-review?pageId='+productCode+'&merchantGroupId='+merchantGroupId+'&merchantId='+merchantId+'&siteId='+siteId+'&zipLocation='+zip_location);
         			$("#PRInlineRating-"+productCode).find("#pr-snippet-rating-decimal").text("0.0");
@@ -84,10 +85,12 @@ define(['modules/jquery-mozu', 'hyprlive',"modules/backbone-mozu",  "modules/mod
             var isReviewDisplay = $("#productReviewDisplay").val() == 1;
             var isSocialAnswerDisplay = $("#productSocialAnswerDisplay").val() == 1;
             var isTabEnabled = $("#reviewSocialAnswerTab").val() == 1;
-            var locale=data.locale;
+            if(data.locale!==null)
+                locale=data.locale;
+           
             merchantGroupId=data.merchantGrpId;
-			
-            merchantId=data.merchantId;
+			merchantId=data.merchantId;
+            
             if(data.merchantSiteId!==null){
                siteId=data.merchantSiteId;
             }
@@ -228,24 +231,6 @@ define(['modules/jquery-mozu', 'hyprlive',"modules/backbone-mozu",  "modules/mod
 	};
       
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
