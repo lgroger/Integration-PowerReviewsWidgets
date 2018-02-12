b1454eda08e1b9eeb9ffcb136ae294766bb6e482
Lori Groger <lgroger@shindigz.com>
2018-02-12 14:51:34 -0500

    remove commented code

--- scripts/pages/product.js
+++ scripts/pages/product.js
@@ -19,12 +19,7 @@ function ($, _, Hypr, CartMonitor, ProductModels, SoftCart, ProductView,PowerRev
             model: product,
             messagesEl: $('[data-mz-message-bar]')
         }); // this calls productView.initialize;
-	/*	
-        var productImagesView = new ProductImageViews.ProductPageImagesView({
-            el: $('[data-mz-productimages]'),
-            model: product
-        });
-*/
+
 		 // extend afterRender() to handle alternate images and power reviews display custom to PDP (not used by quick view)
 		 productView.customAfterRender = function(){
 			//console.log('customAfterRender');
@@ -59,159 +54,15 @@ function ($, _, Hypr, CartMonitor, ProductModels, SoftCart, ProductView,PowerRev
 		return productView;
     };
 	
-	// get product info of all items used as extras - http.commerce.catalog.storefront.products.getProduct.after (application Pricing_Arc_Prod) returns extras as http header "productExtras"
-	/*
-    var getExtrasProductDetails= function(product){
-        var api = Api;
-        api.on('success', function(res, xhr,request){
-            try{
-                var productExtrasResponse = xhr.getResponseHeader("productExtras");
-                if(productExtrasResponse && productExtrasResponse!==""){
-                    var productExtras = JSON.parse(productExtrasResponse);
-                    if(productExtras.length>0){
-                        for(var i=0;i<productExtras.length;i++){
-                            var dndCode = getPropteryByAttributeFQN(productExtras[i], productAttributes.dndCode);
-                            if(dndCode){
-                                $('.addToCart').html('Personalize').addClass('personalize').removeAttr('id').attr('disabled',true);
-                            }
-                            window.extrasProducts.push(productExtras[i]);
-                        }
-
-                    }
-
-                }
-            }catch(e){
-                console.log(e);
-            }
-        }); 
-        api.request('get','/api/commerce/catalog/storefront/products/'+product.get('productCode')+'?my=1').then(function(res){
-			var product=new ProductModels.Product(res);
-                initProductView(product);
-        }); 
-    }; */
-	
-	// get product info of bundle items with stardard productUsage (can get multiple at a time since they are available for sale on their own)
-/*    function getStandardProductDetails(productCodes){
-        if(productCodes.length>0){
-            var filter = '';
-            for(var i= 0; i<productCodes.length;i++){
-                if(i > 0)
-                    filter+=' or productCode eq '+productCodes[i];
-                else
-                   filter+='productCode eq '+productCodes[i];
-            }
-            Api.get('products',{filter:filter}).then(function(res){
-                if(res.length>0){
-                    for(var i=0;i<res.length;i++){
-                        var product=new ProductModels.Product(res[i].data);
-                        var uom = getPropteryValueByAttributeFQN(product, productAttributes.unitOfMeasure);
-                        if(uom===null){
-                            uom = '';
-                        }
-                        //var price = '$'+product.get('price').get('price')+" "+uom;
-                        $('[productcode="'+product.get('productCode')+'"]').find('.uom').html(uom).show();
-						
-						var mcCode = getPropteryValueByAttributeFQN(product, productAttributes.mcCode); // mediaclip code
-                        var dndCode = getPropteryValueByAttributeFQN(product, productAttributes.dndCode);
-                        if(dndCode || mcCode){
-                            window.personalizeBundleProducts.push(product);
-                        }
-                    }
-                }
-                getBundleProductDetails(BundleItems);
-
-            });
-        }else{
-            getBundleProductDetails(BundleItems);
-        }
-    } */
-	/*
-	// get product info of bundle items with component productUsage (can only get 1 at a time in api since we are using storefront)
-    function getBundleProductDetails(arr){
-        if(BundleItems.length>0){
-                Api.get('product',{"productCode":arr[loopcounter]}).then(function(res){
-                    //console.log(res);
-                    var product = new ProductModels.Product(res.data);
-                    var productImage = product.get('content.productImages');
-                    if(productImage.length>0){
-                        $('[productcode="'+arr[loopcounter]+'"]').find('.block-img-sec').html('<img class="bundle-img" src="'+productImage[0].imageUrl+'?max=100"/>');
-                    }
-                    var uom = getPropteryValueByAttributeFQN(product, productAttributes.unitOfMeasure);
-                    if(uom===null){
-                        uom = '';
-                    }
-                    //var price = '$'+product.get('price').get('price')+" "+uom;
-                    $('[productcode="'+arr[loopcounter]+'"]').find('.uom').html(uom).show();
-
-                    var mcCode = getPropteryValueByAttributeFQN(product, productAttributes.mcCode); // mediaclip code
-					var dndCode = getPropteryValueByAttributeFQN(product, productAttributes.dndCode);
-                    if(dndCode || mcCode){
-                        window.personalizeBundleProducts.push(product);
-                    }
-                    loopcounter++;
-                    if(loopcounter < arr.length){
-                        getBundleProductDetails(arr);
-                    }else{
-						
-                     //   if(isPersonalize){
-                      //      $('.addToCart').html('Personalize').addClass('personalize').removeAttr('id');
-                      //  }else{
-                     //       $('.addToCart').attr('disabled',true).removeClass('is-disabled');
-                    //   } 
-                        window.removePageLoader();
-                    }
-                });
-        }else{
-			
-            // if(isPersonalize){
-             //       $('.addToCart').html('Personalize').addClass('personalize').removeAttr('id');
-           // }
-            window.removePageLoader();
-
-        }
-    }
-*/
-
-
-    
-
-		
         var product = ProductModels.Product.fromCurrent();
         
 		var bp = product.get('bundledProducts');
 		for (var i =0; i< bp.length; i++) {
 		   //console.log(bp[i]);
 		}
-		
-		/*  need to refigure...
-        var BundleSections = $('.bundle-block').find('[productcode]');
 
-        BundleSections.each(function(){
-            if($(this).find('input[name="productUsage"]').length===0){
-                BundleItems.push($(this).attr('productCode'));
-            }else{
-                standardProducts.push($(this).attr('productCode'));
-            }
-        });
-		
-        if(standardProducts.length > 0 || BundleItems.length>0){
-            window.showPageLoader();
-            getStandardProductDetails(standardProducts);
-        } */
-		
-		//window.showPageLoader();
 	
 		var productView = initProductView(product);
-		
-        /*var options = product.get('options').toJSON();
-        var extrasProductCodes = [];
-        for(var ind =0 ; ind < options.length; ind++){
-            if(options[ind].attributeDetail.usageType==='Extra' && options[ind].attributeDetail.dataType==='ProductCode'){
-                for(var ind1=0; ind1 < options[ind].values.length; ind1++){
-                    extrasProductCodes.push(options[ind].values[ind1].value);
-                }
-            }
-        }*/
 	
 	$(document).ready(function () {
       
