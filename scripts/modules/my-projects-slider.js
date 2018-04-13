dc2ff0a9c292e2e6ccf32a6661ba3516f136af38
Lori Groger <lgroger@shindigz.com>
2018-04-13 11:24:46 -0400

    added divs to cart projects slider for Gretchen

--- scripts/modules/my-projects-slider.js
+++ scripts/modules/my-projects-slider.js
@@ -1,17 +1,19 @@
 define(['modules/jquery-mozu', "modules/mc-cookie"],function($,McCookie){ 
 
     var projectsCallback = function(res){
-		var $projects = $("#mcProjects");
+        var $projects = $("#mcProjects");
 		var $noprojects = $("#mcProjects-none");
         
 		if(res && res.projects){
 			if(res.projects.length){
+                var $projectsOuter = $("<div />").attr("class","mc-projects-outer");
                 var $projectHolder = $("<div />").attr("class","pdp-related-products");
                 for(var i=0;i<res.projects.length;i++){
                     var p = res.projects[i];
-                    var $project,date = new Date(p.createdDateUtc);
-                    $project = $("<div />").attr("class","mz-productlist-item").attr("data-mc-project",p.id);
-                
+                    var $project,$projectOuter,date = new Date(p.createdDateUtc);
+                    $projectOuter = $("<div />").attr("class","mz-productlist-item");
+                    $project = $("<div />").attr("class","mz-productlist-item-inner").attr("data-mc-project",p.id);
+
                     var $projectInner = $("<div />").attr("class","mz-productlisting mz-productlist-tiled");
                     $project.append($projectInner);
                     var $productImage = $("<a />").attr("href","/p/"+p.entityContainer.item.productCode).append($('<img src="'+p.urlThumb+'" />').attr("title",p.id));
@@ -27,11 +29,13 @@ define(['modules/jquery-mozu', "modules/mc-cookie"],function($,McCookie){
                     $productInfo.append($('<button class="mc-project-atc">Edit / Add to Cart</button>'));
                     $productInfo.append($('<button class="delete-mc-project">Delete</button>'));
                     $productInfo.append($('<button class="copy-mc-project">Copy</button>'));
-                    $projectHolder.append($project);
+                    $projectOuter.append($project);
+                    $projectHolder.append($projectOuter);
                 }
                 $projects.append($("<h2>My Projects</h2>"));
                 // $projectHolder.append($('<div style="clear:both" />'));
-                $projects.append($projectHolder);
+                $projectsOuter.append($projectHolder);
+                $projects.append($projectsOuter);
 
                 $projectHolder.owlCarousel({
                     loop:true, margin:10, nav:true, responsive:{0:{items:2}, 600:{items:2}, 1000:{items:4}}
